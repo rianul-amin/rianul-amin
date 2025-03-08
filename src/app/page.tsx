d36ae28767db18881b6890a1edd994c6484127d7
Rianul Amin Rian <rianulamin.r@Rianuls-MacBook-Air.local>
2025-03-08 10:34:15 +0600

    smooth scrolling added with lenis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,41 @@
+"use client";
+import { useEffect, useRef } from "react";
+import Lenis from "lenis";
 import FirstSection from "@/components/LandingPage/FirstSection";
 
 export default function Home() {
+  const scrollContainerRef = useRef<HTMLDivElement>(null); 
+
+  useEffect(() => {
+    if (!scrollContainerRef.current) return;
+
+    const lenis = new Lenis({
+      duration: 1.5,  
+      wrapper: scrollContainerRef.current,
+      content: scrollContainerRef.current, 
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy(); 
+    };
+  }, []);
+
   return (
-    <div className="lg:h-[calc(100vh-60px)] h-[calc(100vh-160px)] w-full overflow-y-auto grid gap-[5px]">
-      <FirstSection></FirstSection>
-      <FirstSection></FirstSection>
-      <FirstSection></FirstSection>
-      <FirstSection></FirstSection>
+    <div
+      ref={scrollContainerRef}
+      className="lg:h-[calc(100vh-60px)] h-[calc(100vh-160px)] w-full overflow-y-auto grid gap-[5px]"
+    >
+      <FirstSection />
+      <FirstSection />
+      <FirstSection />
+      <FirstSection />
     </div>
   );
 }
