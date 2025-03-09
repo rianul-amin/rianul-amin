c3a0410541bccefa10963a5813ce190a33b552c6
Rianul Amin Rian <rianulamin.r@Rianuls-MacBook-Air.local>
2025-03-09 11:33:13 +0600

    build errors fixed

--- src/components/LandingPage/SecondSection.tsx
+++ src/components/LandingPage/SecondSection.tsx
@@ -1,8 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import { AnimatePresence } from 'framer-motion'
-import { motion } from 'framer-motion'
-import { slideRight, slideUp } from '@/styles/animations'
+import React from 'react'
 
 function SecondSection() {
     return (
